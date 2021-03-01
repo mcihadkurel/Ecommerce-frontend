8caa084898e8e5ff933f0e05137dd6656ed6bed8
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2021-03-01 19:23:06 +0100

    İmport products from strapi and styling

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
-
 import products from "../products.json";
+import { fromImageToUrl } from "../utils/urls";
+import { twoDecimals } from "../utils/format";
 
 export default function Home() {
   return (
@@ -12,8 +14,19 @@ export default function Home() {
       </Head>
 
       {products.map((product) => (
-        <div key={product.name} className=a{styles.product}>
-          {product.name} {product.price}
+        <div key={product.name} className={styles.product}>
+          <Link href={`/products/${product.slug}`}>
+            <a>
+              <div className={styles.product__Row}>
+                <div className={styles.product__ColImg}>
+                  <img src={fromImageToUrl(product.image)} />
+                </div>
+                <div className={styles.product__Col}>
+                  {product.name} ${twoDecimals(product.price)}
+                </div>
+              </div>
+            </a>
+          </Link>
         </div>
       ))}
     </div>
