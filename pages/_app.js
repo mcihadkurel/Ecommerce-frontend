7b4caf9a2d5261a188c4d66d01795d6c8d851f31
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2021-03-15 21:57:56 +0100

    Update Authprovider

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,19 @@
-import "../styles/globals.css";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import '../styles/globals.css'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
-import AuthProvider from './authprovider'
+import { AuthProvider } from '../context/AuthContext'
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
-       <content>
-         <Header />
-         <Component {...pageProps} />
-         <Footer />
-       </content>
-     </AuthProvider>
-   );
+      <content>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </content>
+    </AuthProvider>
+  )
 }
 
-export default MyApp;
-   
+export default MyApp
