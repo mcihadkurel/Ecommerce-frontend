275126a7cbe03da12bd41e881f1543681ca449de
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2021-03-16 14:09:50 +0100

    Stripe url token

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 
+import BuyButton from "../../components/BuyButton";
+
 import { fromImageToUrl, API_URL } from "../../utils/urls";
 import { twoDecimals } from "../../utils/format";
 
@@ -7,48 +9,43 @@ const Product = ({ product }) => {
   return (
     <div>
       <Head>
-            {product.meta_title &&
-                <title>{product.meta_title}</title>
-            }
-            {product.meta_description &&
-                <meta name="description" 
-                content={product.meta_description}
-                />
-            }
-        
-        </Head>
-        <h3>{product.name}</h3>
+        {product.meta_title && <title>{product.meta_title}</title>}
+        {product.meta_description && (
+          <meta name="description" content={product.meta_description} />
+        )}
+      </Head>
+
+      <h3>{product.name}</h3>
       <img src={fromImageToUrl(product.image)} />
       <h3>{product.name}</h3>
-      <p>
-      ${twoDecimals(product.price)}
-      </p>
+      <BuyButton product={product} />
+      <p>${twoDecimals(product.price)}</p>
 
       <p>{product.content}</p>
     </div>
   );
 };
 
-export async function getStaticProps({params: {slug}}) {
-  const product_res = await fetch(`${API_URL}/products/?slug=${slug}`)
-  const found = await product_res.json()
+export async function getStaticProps({ params: { slug } }) {
+  const product_res = await fetch(`${API_URL}/products/?slug=${slug}`);
+  const found = await product_res.json();
 
   return {
     props: {
-        product: found[0]
-    }
-  }
+      product: found[0],
+    },
+  };
 }
 
 export async function getStaticPaths() {
   // Get external data from the file system, API, DB, etc.
-  const products_res = await fetch(`${API_URL}/products`)
-  const products = await products_res.json()
+  const products_res = await fetch(`${API_URL}/products`);
+  const products = await products_res.json();
   return {
-      paths: products.map(el => ({
-          params: {slug: String(el.slug)}
-      })),
-      fallback: false
+    paths: products.map((el) => ({
+      params: { slug: String(el.slug) },
+    })),
+    fallback: false,
   };
 }
 
--- utils/urls.js
+++ utils/urls.js
@@ -1,6 +1,10 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
-export const MAGIC_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAGIC_PUBLIC_KEY || 'pk_test_1B3FA6AE3FE2CDB7'
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
+export const MAGIC_PUBLIC_KEY =
+  process.env.NEXT_PUBLIC_MAGIC_PUBLIC_KEY || "pk_test_1B3FA6AE3FE2CDB7";
 
+export const STRIPE_PK =
+  process.env.NEXT_PUBLIC_STRIPE_PK || "pk_test_51IVY7iGZTI6mYHWyHHTcMm4d1RZRcF6VkTMAmdFC09iG9cFGzqbs9mgKSYBsyXoHVF43uLe8envSVxOsKBkM3Egx00THySoHcm";
 /**
  *
  * @param {any} image
